969fa0550ef07d11b6608b3e74acc0b1909a52e8
HanJunGyo <127172335+Ready-Bridge@users.noreply.github.com>
2023-08-04 20:55:02 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 3040 ms, Memory: 39504 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,27 @@
+import sys
+
+arr = [True for _ in range(1000001)]
+
+for i in range(2, 1001) :
+  if arr[i] :
+    for j in range(2 * i, 1000001, i) :
+      arr[j] = False
+
+
+while(True) :
+  a = int(sys.stdin.readline())
+
+  if a == 0 :
+    break
+
+  flag = 1
+
+  for i in range(3, a, 2) :
+
+    if arr[i] and arr[a - i] :
+      print("%d = %d + %d"%( a, i, a - i))
+      flag = 0
+      break
+    
+  if flag :
+    print("Goldbach's conjecture is wrong.")
