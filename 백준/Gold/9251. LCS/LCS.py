412be7f66a096bc0d691e6ee0b5d7347c76ca38a
HanJunGyo <127172335+Ready-Bridge@users.noreply.github.com>
2024-10-21 21:57:00 +0900

    [Gold V] Title: LCS, Time: 128 ms, Memory: 118228 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,20 +1,20 @@
-import sys
+a = input()
+b = input()
 
-X = sys.stdin.readline().strip()
-Y = sys.stdin.readline().strip()
+x = len(a)
+y = len(b)
 
-m, n = len(X), len(Y)
+dp = [[0] * (y + 1) for _ in range(x + 1)]
 
-dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-for i in range(m) :
-    for k in range(n) :
-        
-        if X[i] == Y[k] :
+for i in range(x) :
+    for k in range(y) :
+
+        if a[i] == b[k] :
             dp[i][k] = dp[i - 1][k - 1] + 1
         
         else :
-            dp[i][k] = max(dp[i - 1][k] , dp[i][k - 1])
+            dp[i][k] = max(dp[i - 1][k], dp[i][k - 1])
     
 
-print(dp[m - 1][n - 1])
+print(dp[x - 1][y - 1])
