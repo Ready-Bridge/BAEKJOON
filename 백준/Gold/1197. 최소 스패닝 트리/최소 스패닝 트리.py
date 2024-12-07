116c922bcb4e3572836122c4c1d703219c40fb6a
HanJunGyo <127172335+Ready-Bridge@users.noreply.github.com>
2024-12-08 02:22:19 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 496 ms, Memory: 137536 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,44 +1,41 @@
-import sys
-sys.setrecursionlimit(10 ** 6)
+# 프림
 
-def find(u) :
-    if parents[u] == u :
-        return u
-    
-    parents[u] = find(parents[u])
-    return parents[u]
+import heapq
 
-def union(u, v) :
-    ur = find(u)
-    vr = find(v)
-
-    if ur != vr :
-        if ur < vr :
-            parents[vr] = ur
-        
-        else :
-            parents[ur] = vr
-
-v, e = map(int, input().split())
+V, E = map(int, input().split())
 
 mst = []
-edges = []
-parents = [x for x in range(v + 1)]
+Q = [] # min-heap (후보 간선 관리)
+S = set()
+neighbors = [[] for _ in range(V + 1)] 
+# heap에 시작 노드 넣기
+heapq.heappush(Q, (0, 1, None)) # weight, node, MST안의 연결된 node
 
-for _ in range(e) :
-    a, b, c = map(int, input().split())
-    edges.append((a, b, c))
+for _ in range(E) :
+    u, v, w = map(int, input().split())
+    
+    neighbors[u].append((v, w))
+    neighbors[v].append((u, w))
 
-edges.sort(key = lambda x : x[2])
+while Q :
+    w, u, v = heapq.heappop(Q)
 
-for u, v, w in edges :
-    if find(u) != find(v) :
-        mst.append((u, v, w))
-        union(u, v)
+    # 이미 u가 MST안에 들어갔다는 뜻
+    if u in S :
+        continue
 
-sum = 0
+    S.add(u)
+    mst.append((v, u, w))
 
-for x in mst :
-    sum += x[2]
+    for v, w_uv in neighbors[u] :
 
-print(sum)
+        if v not in S :
+            heapq.heappush(Q, (w_uv, v, u))
+
+
+cnt = 0 
+
+for i in mst :
+    cnt += i[2]
+
+print(cnt)
