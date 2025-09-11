1cadc66dc94861df1bf8e9c63b05a153d4f59340
HanJunGyo <127172335+Ready-Bridge@users.noreply.github.com>
2025-09-12 00:13:15 +0900

    [level 2] Title: 피보나치 수, Time: 4.06 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    
+    int [] mem = new int[100001];
+    
+    private int fibonacci(int n) {
+        if(mem[n] != -1) {
+            return mem[n];
+        }
+        
+        if(n == 0 || n == 1) {
+            return n;
+        } else {
+            return mem[n] = (fibonacci(n - 1) + fibonacci(n - 2)) % 1234567;
+        }
+    }
+    
+    public int solution(int n) {
+        
+        Arrays.fill(mem, -1);
+        
+        for(int i = 0; i <= n; i++) {
+            fibonacci(i);
+        }
+        
+        return fibonacci(n);
+    }
+}
