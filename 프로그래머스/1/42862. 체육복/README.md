057ca766c04861a41e28b1c8469cfb0abda59e97
HanJunGyo <127172335+Ready-Bridge@users.noreply.github.com>
2025-10-19 23:51:03 +0900

    [level 1] Title: 체육복, Time: 0.14 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?gad_source=1&gad_campaignid=22356298761&gbraid=0AAAAAC_c4nC1_EeEapv96-c1a45LAUjly&gclid=CjwKCAjwmNLHBhA4EiwA3ts3mW_LkREkl7bJYd44A_gWXwsWOdesKlpU6Zut-Xt1uOAxJohOT4a52xoCjIgQAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 91.3 MB, 시간: 0.51 ms
+메모리: 88.3 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 17일 23:50:47
+2025년 10월 19일 23:51:00
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,35 +1,60 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
         
-        Arrays.sort(reserve);
-        Arrays.sort(lost);
-        
-        answer = n - lost.length;
-        
-        for (int i = 0; i < lost.length; i++) {
-			for (int j = 0; j < reserve.length; j++) {
-				if (lost[i] == reserve[j]) {
-					answer++;
-					lost[i] = -1;
-					reserve[j] = -1;
+        // 여분 체육복 있는 학생들중 도난당한 학생들 관련 처리
+        for(int i = 0; i < lost.length; i++) {
+            for(int k = 0; k < reserve.length; k++) {
+                
+                // 도난 당했으면
+                if(lost[i] == reserve[k]) {
+                    lost[i] = -1;
+                    reserve[k] = -1;
                     break;
-				}
-			}
-		}
+                }
+            }
+        }
         
-        for (int i = 0; i < lost.length; i++) {
-			for (int j = 0; j < reserve.length; j++) {
-				if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
-					answer++;
-					reserve[j] = -1;
-					break;
-				}
-			}
-		}
+        List<Integer> lostList = new ArrayList<>();
+        List<Integer> reserveList = new ArrayList<>();
         
-        return answer;
+        // List에 lost와 reserve를 갱신하고 오름차순 처리 
+        for(int i = 0; i < lost.length; i++) {
+            if(lost[i] != -1) {
+                lostList.add(lost[i]);
+            }
+        }
+        
+                // List에 lost와 reserve를 갱신하고 오름차순 처리 
+        for(int i = 0; i < reserve.length; i++) {
+            if(reserve[i] != -1) {
+                reserveList.add(reserve[i]);
+            }
+        }
+        
+        Collections.sort(lostList);
+        Collections.sort(reserveList);
+        
+        // lost를 돌면서 reserve에 빌려줄 수 있는 사람이 있으면 바로 빌려주면서 List 갱신
+        for(int i = 0; i < lostList.size(); i++) {
+            
+            int tempLost = lostList.get(i);
+            
+            for(int k = 0; k < reserveList.size(); k++) {
+                
+                int tempReserve = reserveList.get(k);
+                
+                // 빌려줄 수 있다면 List에서 해당 학생들을 삭제 (인덱스도 하나씩 내려줌)
+                if(tempLost + 1 == tempReserve || tempLost - 1 == tempReserve) {
+                    lostList.remove(i--);
+                    reserveList.remove(k--);
+                    break;
+                }
+            }
+        }
+        
+        return n - lostList.size();
+        
     }
 }
