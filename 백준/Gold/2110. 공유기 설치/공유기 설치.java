132f5582050cd2b18f633120521b241e58cf284b
HanJunGyo <127172335+Ready-Bridge@users.noreply.github.com>
2026-01-27 19:33:27 +0900

    [Gold IV] Title: 공유기 설치, Time: 296 ms, Memory: 29676 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int left = 1; 
+        int right = arr[N - 1] - arr[0];
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2; 
+            
+            int count = 1;
+            int lastInstalled = arr[0];
+
+            for (int i = 1; i < N; i++) {
+                if (arr[i] - lastInstalled >= mid) {
+                    count++;
+                    lastInstalled = arr[i];
+                }
+            }
+            
+            if (count >= C) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
