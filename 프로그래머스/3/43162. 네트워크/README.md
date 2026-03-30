42cc23e5bfe141d49fd07d2eceae209f921326f4
HanJunGyo <127172335+Ready-Bridge@users.noreply.github.com>
2026-03-31 08:57:32 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?gad_source=1&gad_campaignid=22356298761&gbraid=0AAAAAC_c4nAdKURnhJMsf8fhubXW7jKCO&gclid=CjwKCAjwvqjOBhAGEiwAngeQnXkUbxfqXTWxCYKnBARVjUIm1b-CqessloN18smVoRp0jRA5rUFQJBoCPZ0QAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 80.4 MB, 시간: 0.47 ms
+메모리: 74.9 MB, 시간: 0.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 17일 22:27:40
+2026년 03월 31일 08:57:29
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,36 +1,32 @@
-import java.util.*;
-
 class Solution {
+    
+    int answer;
+    
     public int solution(int n, int[][] computers) {
+        answer = 0;
+        boolean[] visited = new boolean[n]; // 방문 여부 체크
         
-        int answer = 0;
-        boolean[] visited = new boolean[n];
-        Queue<Integer> q = new LinkedList<>();
-        
-        for(int i = 0; i < n; i++) {
-            
-            if(!visited[i]) {
-                q.add(i);
-                visited[i] = true;   
-                answer++;
+        // 0번 컴퓨터부터 마지막 컴퓨터까지 순회
+        for (int i = 0; i < n; i++) {
+            // 아직 방문하지 않은 컴퓨터를 만났다면 새로운 덩어리 발견!
+            if (!visited[i]) {
+                answer++; // 네트워크 개수 1 증가
+                dfs(i, computers, visited); // 연결된 모든 컴퓨터를 찾아 방문 처리
             }
-            
-            while(!q.isEmpty()) {
-            
-                int computer = q.poll();
-                
-                for(int k = 0; k < n; k++) {
-                    
-                    if(!visited[k] && computers[computer][k] == 1) {
-                        q.add(k);
-                        visited[k] = true;
-                    }
-                }
-            }
-            
         }
         
         return answer;
+    }
     
+    private void dfs(int node, int[][] computers, boolean[] visited) {
+        visited[node] = true; // 현재 노드 방문 처리
+        
+        // 인접 행렬을 돌며 현재 노드와 연결된 다른 노드를 찾음
+        for (int i = 0; i < computers.length; i++) {
+            // 나 자신이 아니고, 연결되어 있으며, 아직 방문 안 한 노드라면 파고들기
+            if (i != node && computers[node][i] == 1 && !visited[i]) {
+                dfs(i, computers, visited); 
+            }
+        }
     }
 }
